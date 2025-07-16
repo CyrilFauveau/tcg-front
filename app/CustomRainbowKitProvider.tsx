c46c37ee568065b63a1124455e07b03cdb54e377
Cyril Fauveau <fauveaucyril@gmail.com>
2025-07-16 11:55:21 +0200

    build: add rainbowkitprovider

---
+++ app/CustomRainbowKitProvider.tsx
@@ -0,0 +1,28 @@
+'use client';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'b8a23a63eef373200d80f8f84315e935',
+    chains: [sepolia],
+    ssr: true,
+});
+
+const queryClient = new QueryClient();
+const CustomRainbowKitProvider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+                {children}
+            </RainbowKitProvider>
+        </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default CustomRainbowKitProvider;
